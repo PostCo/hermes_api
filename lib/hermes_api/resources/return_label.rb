0b1a07517aabc076ff6fca98b2d79b127b843f30
Andy Chong <andy_gg1996@hotmail.com>
2021-08-03 13:29:35 +0800

    Attach print_in_store_qr function to return_label

--- lib/hermes_api/resources/return_label.rb
+++ lib/hermes_api/resources/return_label.rb
@@ -50,6 +50,8 @@ module HermesAPI
     rescue NameError
     end
 
+    alias_method :label, :labels
+
     def tracking_numbers
       entries = routingResponseEntries.routingResponseEntry
       if entries.is_a?(Array)
@@ -62,7 +64,65 @@ module HermesAPI
     rescue NameError
     end
 
-    alias_method :label, :labels
     alias_method :tracking_number, :tracking_numbers
+
+    def request_print_in_store_qr_code(attrs = {
+      dimensions: {
+        depth: 15,
+        length: 20,
+        width: 15,
+        weight: 1
+      },
+      value: {
+        currency: "GBP",
+        amount: 10
+      }
+    })
+      return nil if attributes["routingResponseEntries"].blank?
+
+      entries = routingResponseEntries.routingResponseEntry
+      entry = entries.is_a?(Array) ? entries[0] : entries
+      carrier = entry.inboundCarriers.carrier1
+      barcode = carrier.barcode1
+      customer = collectionRoutingRequestEntries[0].customer
+      address = customer.address
+
+      self.print_in_store_qr_code = PrintInStoreQr.create(
+        customer: {
+          customerReference1: customer.customerReference1
+        },
+        deliveryAddress: {
+          name: "#{address.firstName} #{address.lastName}",
+          addressLine1: address.addressLine1,
+          countryCode: address.countryCode,
+          postcode: address.postCode
+        },
+        labelType: "RETURN",
+        barcode: {
+          barcode: barcode.barcodeNumber,
+          barcodeDisplay: barcode.barcodeDisplay
+        },
+        client: {
+          clientId: clientId,
+          clientName: clientName
+        },
+        routing: {
+          deliveryMethod: {
+            deliveryMethodId: carrier.deliveryMethodCode,
+            deliveryMethodDescription: carrier.deliveryMethodDesc
+          },
+          sortLevels: {
+            sortLevel1: carrier.sortLevel1.strip,
+            sortLevel2: carrier.sortLevel2
+          }
+        },
+        serviceOffers: [],
+        **attrs
+      )
+    end
+
+    def base64_print_in_store_qr_code
+      as_json.dig("print_in_store_qr_code", "qrCode", "base64EncodedBytes")
+    end
   end
 end
